65efbed94580662f982045c08dd4cdbf33299f85
Krishna Pillutla <knpillutla@gmail.com>
2018-11-06 11:28:35 -0500

    adding

---
+++ src/main/java/com/threedsoft/inventory/dto/requests/InventorySearchRequestDTO.java
@@ -0,0 +1,30 @@
+package com.threedsoft.inventory.dto.requests;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.threedsoft.util.dto.WMSRequestDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonAutoDetect(fieldVisibility = Visibility.ANY)
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class InventorySearchRequestDTO extends WMSRequestDTO{
+	public String busName;
+	public Integer locnNbr;
+	public String busUnit;
+	public String company;
+	public String division;
+	public String locnBrcd;
+	public String itemBrcd;
+	public Integer qty;
+	public String ilpn;
+	public String trackByLPN;
+	public String userId;
+	public String locked;
+}
